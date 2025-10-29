86d3e61e3ce032f6ea3619dda49fef0e6b5a6776
yogeshkumar0 <yogeshkumar0@gmail.com>
2025-10-29 08:48:52 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/hello-world-react-vite/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+});
